64df5b9fec2dbcae387b329c8e3f7e49326df6e9
jabiologo <javfer05@ucm.es>
2017-08-21 15:05:52 -0400

    add sea currents functions

--- R/wind_functions.R
+++ R/wind_functions.R
@@ -219,4 +219,92 @@ flow.dispersion <-function(dl, sl, type="passive", output="raw"){
 }
 
 
+sea.dl<- function (yyyy, mm, dd, lon1, lon2, lat1, lat2, type = "read-data", ...) {
+
+  mm <- sprintf("%02d", mm)
+  dd <- sprintf("%02d", dd)
+
+
+  url_dir <- paste("http://coastwatch.pfeg.noaa.gov/erddap/griddap/erdTAgeo1day_LonPM180.csv?u_current[(",
+                   yyyy,"-",mm,"-",dd,"T12:00:00Z)][(0.0)][(",lat1,"):(",lat2,")][(",lon1,"):(",lon2,
+                   ")],v_current[(",yyyy,"-",mm,"-",dd,"T12:00:00Z)][(0.0)][(",lat1,"):(",lat2,")][(",lon1,"):(",lon2,
+                   ")]&.draw=vectors&.vars=longitude%7Clatitude%7Cu_current%7Cv_current&.color=0x000000&.bgColor=0xffccccff",
+                   sep = "")
+  if (type == "csv") {
+    download.file(url_dir, paste("wind_", yyyy, "_",
+                                 mm, "_", dd, "_", tt, ".csv", sep = ""))
+  }
+  else {
+    return(read.csv(url_dir, ...))
+  }
+}
+
+sea.fit<- function(X){
+
+  rad2deg <- function(rad) {
+    (rad * 180)/(pi)
+  }
+
+  bruto <- X
+  bruto <- data.frame(bruto[2:nrow(bruto), ])
+  bruto <- bruto[, 3:ncol(bruto)]
+  indx <- sapply(bruto, is.factor)
+  bruto[indx] <- lapply(bruto[indx], function(x) as.numeric(as.character(x)))
+  big_bruto <- data.frame(1:nrow(bruto))
+
+  names(bruto) <- c("lat", "lon", "ugrd10m", "vgrd10m")
+  big_bruto <- cbind(big_bruto, bruto)
+  big_bruto_dir <- data.frame(1:nrow(bruto))
+  names(big_bruto_dir) <- "V1"
+  big_bruto_speed <- data.frame(1:nrow(bruto))
+  names(big_bruto_speed) <- "V1"
+  for (g in 1:1) {
+    u <- "ugrd10m"
+    v <- "vgrd10m"
+    for (t in 1:nrow(big_bruto)) {
+      nugget <- atan2(big_bruto[t, u], big_bruto[t, v])
+      nugget <- rad2deg(nugget)
+      if (is.nan(nugget)) { big_bruto_dir[t,g] <- NaN}
+      else {if (nugget < 0) {
+        nugget <- 360 + nugget
+      }
+      }
+      big_bruto_dir[t, g] <- nugget
+    }
+  }
+  for (q in 1:1) {
+    u <- "ugrd10m"
+    v <- "vgrd10m"
+    for (w in 1:nrow(big_bruto)) {
+      mcpollo <- sqrt((big_bruto[w, u] * big_bruto[w, u]) +
+                        (big_bruto[w, v] * big_bruto[w, v]))
+      big_bruto_speed[w, q] <- mcpollo
+    }
+  }
+  big_bruto <- cbind(big_bruto[, 2:3], big_bruto_dir, big_bruto_speed)
+  big_bruto <- big_bruto[with(big_bruto, order(-lat)), ]
+  names(big_bruto) <- c("lat", "lon", "dir", "speed")
+  big_bruto$lon<-round(big_bruto$lon,6)
+  big_bruto<-big_bruto[big_bruto$lon != -0.025,]
+  big_bruto$lon[big_bruto$lon < 0] <-(big_bruto$lon[big_bruto$lon < 0]) + 0.025
+
+  return(big_bruto)
+
+}
+
+sea2raster<-function(SC, type="dir"){
+  coordinates(SC) = ~lon+lat
+  SC$speed[is.nan(SC$speed)==TRUE]<--9999
+  SC$dir[is.nan(SC$dir)==TRUE]<--9999
+  rast <- raster(ext=extent(SC), nrows=length(SC$lon[SC$lon == SC$lon[1]]) , ncols=length(SC$lat[SC$lat == SC$lat[1]]))
+
+  if (type == "dir"){
+    r<-rasterize(SC, rast, SC$dir) }
+  else {
+    r<-rasterize(SC, rast, SC$speed)
+  }
+  r[r[]==-9999]<-NA
+  return(r)
+}
+
 
