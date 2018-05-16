08bba0907215f0eef8a57124da87049d4a578503
jabiologo <javfer05@ucm.es>
2018-05-16 21:28:04 +0200

    Changes in test files

--- tests/testthat/test_rWind.R
+++ tests/testthat/test_rWind.R
@@ -7,20 +7,23 @@ colnames(X) <- c("time (UTC)", "latitude (degrees_north)",
 
 w <- wind.fit(X)
 
-dl <- wind2raster(w)
+dl <- wind2raster(w, type="dir")
 sl <- wind2raster(w, type="speed")
 
-fl1 <- flow.dispersion(dl,sl, "passive", "raw")
-fl2 <- flow.dispersion(dl,sl, "active", "raw")
+wind <- wind2raster(w, type="stack")
 
-fl3 <- flow.dispersion(dl,sl, "passive", "transitionLayer")
-fl4 <- flow.dispersion(dl,sl, "active", "transitionLayer")
+fl1 <- flow.dispersion(wind, "passive", "raw")
+fl2 <- flow.dispersion(wind, "active", "raw")
+
+fl3 <- flow.dispersion(wind, "passive", "transitionLayer")
+fl4 <- flow.dispersion(wind, "active", "transitionLayer")
 
 
 test_that("rWind works as expected", {
   expect_is(w, "data.frame")
   expect_is(dl, "RasterLayer")
   expect_is(sl, "RasterLayer")
+  expect_is(wind, "RasterStack")
   expect_is(fl1, "dgCMatrix")
   expect_is(fl3, "TransitionLayer")
 })
