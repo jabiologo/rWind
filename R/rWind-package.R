01787173ce40a3f18283477a265dd3825ff19bb2
jabiologo <javfer05@ucm.es>
2018-06-12 18:18:43 +0200

    including wind.series data and documentation

--- R/rWind-package.R
+++ R/rWind-package.R
@@ -58,3 +58,41 @@ NULL
 NULL
 
 
+#' Wind series example
+#'
+#' This is an example of a wind series data obtained with wind.dl function
+#' for New Zealand area on 3/January/2015 at all the available times:
+#' 00:00 - 03:00 - 06:00 - 09:00 - 12:00 - 15:00 - 18:00 - 21:00 (UTC)
+#'
+#' This data set is the result of:
+#'
+#' \code{wind.series <- wind.dl(2015,1,3,0,2015,1,3,21,164,179,-48,-33)}
+#'
+#' @name wind.series
+#' @docType data
+#' @format The format is an rWind list of 8 data.frame. Each data.frame contain
+#' 961 observations on the following 7 variables:
+#'
+#' \describe{ \item{list("time (UTC)")}{a factor with selected time of wind
+#' data } \item{list("latitude (degrees_north)")}{a factor with latitude values
+#' } \item{list("longitude (degrees_east)")}{a factor with longitude values }
+#' \item{list("ugrd10m (m s-1)")}{a factor with U component of wind data }
+#' \item{list("vgrd10m (m s-1)")}{a factor with V component of wind data }
+#' \item{list("dir")}{a numeric with direction of wind data }
+#' \item{list("speed")}{a numeric with speed of wind data } }
+#'
+#' @references
+#' http://oos.soest.hawaii.edu/erddap/info/NCEP_Global_Best/index.html
+#' @source
+#' http://allthiswasfield.blogspot.com.es/2016/12/rwind-r-package-released.html
+#' @keywords datasets
+#' @examples
+#'
+#' data(wind.series)
+#' str(wind.series)
+#'
+#'
+NULL
+
+
+
